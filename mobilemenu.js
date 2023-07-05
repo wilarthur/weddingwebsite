1a77f5452e401903b20ba5c56e54eebcd4a77050
wilarthur <will.arthur@hotmail.com>
2023-07-05 19:57:15 +0100

    animated mobile menu

--- mobilemenu.js
+++ mobilemenu.js
@@ -1,8 +1,16 @@
-function mobileMenu() {
-  var x = document.getElementById("nav-menu");
+/*function mobileMenu() {
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
   }
+}*/
+
+function mobileMenu() {
+  let x = document.getElementById("nav-menu");
+  let y = document.getElementById("nav-toggle-close");
+  let z = document.getElementById("nav-toggle-open");
+  x.classList.toggle("hidden");
+  y.classList.toggle("hidden");
+  z.classList.toggle("hidden");
 }
